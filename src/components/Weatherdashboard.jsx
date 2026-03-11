9795533fd8be39db9008e603c95624dfb4dcecf8
Anant <anantpathak564@gmail.com>
2026-03-11 20:32:28 +0530

    Weather app continue

--- src/components/Weatherdashboard.jsx
+++ src/components/Weatherdashboard.jsx
@@ -1,33 +1,57 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useSelector } from "react-redux"
 import SunnyIcon from '@mui/icons-material/Sunny';
-
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import "../index.css"
 function Weatherdashboard() {
-    const temp = useSelector(state => state.weather.currentTemp)
+    const obj = useSelector(state => state.weather.currentTemp)
+    const list = useSelector(state=> state.weather.data)
+    const temp = Math.round(obj?.temp)
+    const feels = Math.round(obj?.feels_like)
 
-    return temp ? (
+    const minmaxTemp = ()=>{
+
+        const formatted = new Date().toISOString().split('T')[0]
+        const temps = list?.map((item)=>{
+            console.log(item)
+            console.log(item.dt_txt)
+        if(item.dt_txt.includes(formatted)){
+            console.log(item.dt_txt)
+
+        }
+        }
+        )
+
+    }
+    useEffect(()=>{
+
+    minmaxTemp()
+
+
+    },[])
+
+    return obj ? (
         <>
             <div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-60 rounded-2xl">
-                        <img src="https://as1.ftcdn.net/v2/jpg/01/80/41/10/1000_F_180411044_aP8YqKLbHnaH5cWLTGLhuxWO0zkV8mrY.jpg" alt="Tailwind CSS 3D hover" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
+            <div className="w-[390px] h-[190px] ml-[600px] relative mt-3 border">
+                <div>
+                    <div className="absolute top-[50px] right-1">
+                    <WbSunnyIcon className="text-yellow-300" fontSize="large"/>
+                    <div className="text-white"> <span className="text-[12px]">feels like: </span>{feels}</div>
+                    </div>
+                    <div>
+                    <div className="text-white font-bold absolute top-[70px] left-3">{temp}°C</div>
+                    </div>
+                 
+                 
+                 </div>
 
             </div>
 
-            <SunnyIcon fontSize="large" className="text-yellow-300"/>
+            </div>
 
+            
+           
         </>
     ) : <div>Loading...</div>
 }
